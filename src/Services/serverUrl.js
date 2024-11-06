5e98424cfb2629c4e699b5896c2eac19b34eec8d
Neethu ku <neethuku02@gmail.com>
2024-11-06 14:45:38 +0530

    server url changed

--- src/Services/serverUrl.js
+++ src/Services/serverUrl.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://pf-app-server-tyzh.onrender.com"
+// export const SERVER_URL = "https://pf-app-server-tyzh.onrender.com"
+export const SERVER_URL ="https://pf-app-server-2.onrender.com"
